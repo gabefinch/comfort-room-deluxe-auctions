f285ed04bc8aae295d5887afb277966f8370e659
Gabe Finch and Sarah Joy Calpo <gabefinch@gmail.com>
2015-03-05 11:56:45 -0800

    add delete functionality to items

--- controllers/lot.js
+++ controllers/lot.js
@@ -2,6 +2,10 @@ CRDeluxe.LotController = Ember.ObjectController.extend({
   needs: ['auction'],
   showAdd: true,
   actions: {
+    itemDelete: function(item) {
+      item.destroyRecord();
+      item.save();
+    },
     toggleAdd: function() {
       this.set('showAdd', !this.get('showAdd'));
       this.transitionTo('newItem', this.get('id'));
